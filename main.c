f385eeecb52bf7a2fe5b6870f405ea3e73174367
aimar62 <lijiangbing62@163.com>
2020-05-02 15:56:05 +0800

    ut hash add

--- main.c
+++ main.c
@@ -9,23 +9,11 @@
 #include "AvlTree.h"
 #include "stockSpan_901.h"
 #include "CombineSum3_216.h"
-
+#include "uthash.h"
 
 
 int main()
 {
-    int** out = NULL;
-    int* complsize = NULL;
-    int num = 0;
-    out = combinationSum3(3, 22, &num, &complsize);
-    int i;
-    int j;
-
-    for (i = 0; i < num; i++) {
-        for (j = 0; j < complsize[i]; j++) {
-            printf("%3d", out[i][j]);
-        }
-        printf("\n");
-    }
+    
     return 0;
 }
